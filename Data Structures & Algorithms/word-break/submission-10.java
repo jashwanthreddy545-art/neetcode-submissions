d2c8af536542678310144844895fbf28c816b6f2
jashwanthreddy545-art <jashwanthreddy545@gmail.com>
2026-04-28 23:36:44 +0530

    Add: word-break - submission-10

---
+++ Data Structures & Algorithms/word-break/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        Boolean[] memo = new Boolean[s.length()];
+        return dfs(s, 0, set, memo);
+    }
+
+    private boolean dfs(String s, int start, Set<String> set, Boolean[] memo) {
+        if (start == s.length())
+            return true;
+
+        if (memo[start] != null)
+            return memo[start];
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (set.contains(s.substring(start, end)) && dfs(s, end, set, memo)) {
+                return true;
+            }
+        }
+
+        return memo[start] = false;
+    }
+}
