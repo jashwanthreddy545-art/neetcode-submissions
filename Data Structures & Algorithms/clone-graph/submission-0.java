94341b3933889d6848f9b02e509cf98263bb6f64
jashwanthreddy545-art <jashwanthreddy545@gmail.com>
2026-07-15 23:31:57 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,47 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+
+        return dfs(node);
+    }
+
+    private Node dfs(Node node) {
+        // Already cloned
+        if (map.containsKey(node))
+            return map.get(node);
+
+        // Clone current node
+        Node clone = new Node(node.val);
+
+        map.put(node, clone);
+
+        // Clone neighbors
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor));
+        }
+
+        return clone;
+    }
+}
